01d44a1159c2f0f66be2df2bbf23a2fdcbc674ed
Durga <RADHAKRISHNA_DURGA@homedepot.com>
2017-10-27 16:34:18 -0400

    commit changes

---
+++ src/main/java/com/example/homedepot/supplychain/ipp/testrk/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.homedepot.supplychain.ipp.testrk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("This is a Demo Proj");
+	}
+}
