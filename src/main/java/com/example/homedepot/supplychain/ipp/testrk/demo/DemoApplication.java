2b06fcecf470b6497b1f30a7c04d1094cf2f2b9b
Durga <RADHAKRISHNA_DURGA@homedepot.com>
2017-10-27 16:43:34 -0400

    commit changes

--- src/main/java/com/example/homedepot/supplychain/ipp/testrk/demo/DemoApplication.java
+++ src/main/java/com/example/homedepot/supplychain/ipp/testrk/demo/DemoApplication.java
@@ -9,5 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("This is a Demo Proj");
+		System.out.println("New Branch test");
 	}
 }
